9484d17dc8d631af1e10251b09782975d55c22a1
Marsaju <marcelsarraseca@gmail.com>
2021-10-19 10:20:07 +0200

    Initial commit

---
+++ src/main/java/Barco.java
@@ -0,0 +1,9 @@
+public class Barco {
+
+    boolean alive;
+    int type; //tipo de barco, submarino, portaaviones...
+    int orientation;
+    int size;
+
+
+}
---
+++ src/test/java/BarcoTest.java
@@ -0,0 +1,7 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class BarcoTest {
+
+    IntroduirVaixell(string a, string b)
+
+}
